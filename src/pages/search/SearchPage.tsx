69d070c8f9c31103055645d07f93ebc37371b2ae
me-coderaider <kashyappankajnsp@gmail.com>
2024-11-16 17:31:52 +0530

    connecting 'SearchPage' & 'searchLoader'

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,7 +1,12 @@
+import type { SearchLoaderResult } from "./searchLoader";
 import { useLoaderData } from "react-router-dom";
 
 export default function SearchPage() {
-    const data = useLoaderData();
+    // const data = useLoaderData() as {searchResults:PackageSummary[]}; // type is coming as 'unknown'
+    // above solution will not work for complex projects as what if someone changes 'searchResults' property from the loader function.
+
+    // better solution
+    const data = useLoaderData() as SearchLoaderResult;
     console.log(data);
     return <div>SearchPage</div>;
 }
